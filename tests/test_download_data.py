681029edc8fae2802f729e9b6ceffdbd042f9b9c
Florence Bockting <48919471+FloB2@users.noreply.github.com>
2023-12-20 23:26:14 +0100

    changed path

--- tests/test_download_data.py
+++ tests/test_download_data.py
@@ -13,8 +13,8 @@ def download_data_instance(tmp_path):
     return DownloadData(data_url=data_url, target_path=str(tmp_path), data_file=str(data_file))
 
 # Test the set_cwd method with different target paths
-@pytest.mark.parametrize("target_path", [pathlib.PureWindowsPath(r"example-project\examples\data"),
-                                         pathlib.PureWindowsPath(r"example-project\tests")])
+@pytest.mark.parametrize("target_path", [pathlib.PureWindowsPath(r"examples\data"),
+                                         pathlib.PureWindowsPath(r"tests")])
 def test_set_cwd(download_data_instance, target_path, capsys):
     download_data_instance.set_cwd(target_path)
 

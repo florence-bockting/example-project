9ff3230ffb7ab99bfff8e6ca8f5d012a0a152d7c
Florence Bockting <48919471+FloB2@users.noreply.github.com>
2023-12-21 01:45:45 +0100

    here

--- tests/test_download_data.py
+++ tests/test_download_data.py
@@ -13,11 +13,12 @@ def download_data_instance(tmp_path):
     return DownloadData(data_url=data_url, target_path=str(tmp_path), data_file=str(data_file))
 
 # Test the set_cwd method with different target paths
-@pytest.mark.parametrize("target_path", [os.chdir("~/examples"),
-                                         os.chdir("~/tests")])
+@pytest.mark.parametrize("target_path", [os.chdir("/examples"),
+                                         os.chdir("/tests")])
 
 #@pytest.mark.skip(reason="fails and I don't know why")
 def test_set_cwd(download_data_instance, target_path, capsys):
+    print("HEEEERREEEE", os.getcwd())
     download_data_instance.set_cwd(target_path)
     # Capture the printed output
     captured = capsys.readouterr()

4de15142023b6bf802b67b8a27393c2ae858a329
Florence Bockting <48919471+FloB2@users.noreply.github.com>
2023-12-21 01:28:01 +0100

    print cwd

--- tests/test_download_data.py
+++ tests/test_download_data.py
@@ -19,7 +19,7 @@ def download_data_instance(tmp_path):
 #@pytest.mark.skip(reason="fails and I don't know why")
 def test_set_cwd(download_data_instance, target_path, capsys):
     download_data_instance.set_cwd(target_path)
-
+    print(os.getcwd())
     # Capture the printed output
     captured = capsys.readouterr()
 

31951368e0123ec2b4f0ad390d9dcc43bbb120cf
Florence Bockting <48919471+FloB2@users.noreply.github.com>
2024-01-11 09:02:46 +0100

    final changes in the example-project folder

--- tests/test_expectation.py
+++ tests/test_expectation.py
@@ -2,16 +2,19 @@ import pytest
 import numpy as np
 import example_project.expectation as expectation
 
-@pytest.fixture
+@pytest.fixture(scope = "class")
 def x1():
     return np.random.uniform(0,1,100)
 
 def test_mean(x1):
     assert expectation.mean(x1) == pytest.approx(np.mean(x1))
-  
-@pytest.fixture
-def x2():
-    return np.random.normal(0,1,100)
     
-def test_variance(x2):
-    assert expectation.variance(x2) == pytest.approx(np.var(x2))
+def test_variance(x1):
+    assert expectation.variance(x1) == pytest.approx(np.var(x1))
+    
+
+@pytest.mark.parametrize("x, expected_result", [([2, 3], 2.5), 
+                                                ([-2, -3], -2.5), 
+                                                ([-2, 3], 0.5)])
+def test_mean2(x, expected_result):
+    assert expectation.mean(x) == expected_result
